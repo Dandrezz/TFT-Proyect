2794f3ce81ae43af268d487da704b843ae0caae3
Diego <diegoaporterol@gmail.com>
2020-11-09 01:04:28 -0500

    Fist version

---
+++ src/MainApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomePage from './components/Home/HomePage';
+
+function MainApp() {
+return (
+    <HomePage />    
+  );
+}
+
+export default MainApp;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainApp from './MainApp';
+import './index.css';
+import 'semantic-ui-css/semantic.min.css';
+
+ReactDOM.render(
+    <MainApp />,
+  document.getElementById('root')
+);
+
