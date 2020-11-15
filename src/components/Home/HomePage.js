56431dca375857f64d8e5c0cfcf8771addf48250
Diego <diegoaporterol@gmail.com>
2020-11-14 23:47:11 -0500

    Casi perfecto

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './HomePage.css';
 import listName from '../../data/namesChampions.json';
 
@@ -6,10 +6,36 @@ function HomePage() {
 
     const [names, setNames] = useState([]);
     const [input, setInput] = useState("");
-    let algo = "asd";
-    algo.substr()
+    const [temp, setTemp] = useState("");
+    const inputSearch = useRef();
+    const [mouseOver, setMouseOver] = useState(true);
+    const handleKeyPress = (e) => {
+        setMouseOver(true);
+        if( e.keyCode ===40 ){
+            const index = names.indexOf(input);
+            if( index > (names.length - 2) ){
+                setInput(temp);
+            }else if(index===-1){
+                setTemp(input);
+                setInput(names[0]);
+            }else{
+                setInput(names[ names.indexOf(input) + 1 ]);
+            }
+        }else if( e.keyCode === 38 ){
+            e.preventDefault();
+            const index = names.indexOf(input);
+            if(index===-1){
+                setTemp(input);
+                setInput(names[names.length - 1]);
+            }else if(index===0){
+                setInput(temp);
+            }else{
+                setInput(names[ names.indexOf(input) - 1 ]);
+            }
+        }
+    }
 
-    const handleInputChange = ({ target }) => {
+    const handleInputChange = ({target}) => {
         setInput(target.value);
         if (target.value.length) {
             setNames(listName["names"].filter(item => (item.toLowerCase().includes(target.value.toLowerCase()))).splice(0, 5));
@@ -25,6 +51,11 @@ function HomePage() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log(input);
+    }
+
+    const handleMouseOver = (e) =>{
+        setMouseOver(false);
     }
 
     return (
@@ -43,27 +74,31 @@ function HomePage() {
                         className={`main-input ${names.length !== 0 && "focus-input"}`}
                         onChange={handleInputChange}
                         value={input}
+                        onKeyDown={handleKeyPress}
+                        ref={inputSearch}
                     />
                     {
                         (names.length !== 0) &&
-                        <ul className="suggestions">
+                        <ul className="suggestions"
+                            onMouseOver={handleMouseOver}
+                        >
                             {
                                 names.map((item, i) => {
                                     return (
                                         <li
-                                            className="suggestion-active"
+                                            className={`suggestion-active ${(item===input && mouseOver === true ) && "select"}`}
                                             onClick={handleClickName}
                                             key={i}
                                         >
                                             <span>
-                                                {input}
                                                 <b>
-                                                    {item.substring(input.length, item.length)}
+                                                    {item}
                                                 </b>
                                             </span>
                                         </li>)
                                 })
                             }
+                            <li></li>
                         </ul>
                     }
                 </div>
